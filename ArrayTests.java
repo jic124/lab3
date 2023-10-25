1e9e90a43c242155697bdf39c3d9e7ec73a5f3fd
jic124 <jic124@ucsd.edu>
2023-10-25 14:34:45 -0700

    .

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,38 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test 
+	public void testReverseInPlaceTwo() {
+    int[] input1 = { 1, 2 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 2, 1 }, input1);
+	}
+
+
+
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversedTwo() {
+    int[] input1 = { 1, 2 };
+    assertArrayEquals(new int[]{ 2, 1 }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversedThree() {
+    int[] input1 = { 1, 2, 3 };
+    assertArrayEquals(new int[]{ 3, 2, 1 }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversedFour() {
+    int[] input1 = { 1, 2, 3, 4 };
+    assertArrayEquals(new int[]{ 4, 3, 2, 1 }, ArrayExamples.reversed(input1));
+  }
+
 }
